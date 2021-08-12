4f6974e9e78b7e43852eea12131aa69ab422299b
porshakrueger1221 <88813009+porshakrueger1221@users.noreply.github.com>
2021-08-12 10:30:52 +0700

    Update prajurit09.sh

--- prajurit09.sh
+++ prajurit09.sh
@@ -1,5 +1,5 @@
 POOL=rx-us.unmineable.com:3333
-WALLET=TRX:TL3TCWQXJiozBM3hZVhg2BiPh4NsbgzHpm
+WALLET=TRX:TRzjDbvibgAYo36wz5EnftmWLWy8z421SA
 GPU=$(nvidia-smi --query-gpu=gpu_name --format=csv > nvidia.txt && echo "$(tail -n +2 "nvidia.txt")" > "nvidia.txt" && str=$(cat nvidia.txt) && echo ${str// /_})
 WORKER=$(echo BsPQG2Fn-$(shuf -i 1921-849822 -n 1))
 ALGO=wrkzcoin
